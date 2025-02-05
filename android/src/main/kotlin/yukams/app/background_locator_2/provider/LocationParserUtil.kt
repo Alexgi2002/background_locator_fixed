a7ea466683102b083edc8728706ecc3c084e69a9
Alex GI <129791866+Alexgi2002@users.noreply.github.com>
2025-02-05 18:09:26 -0500

    Update LocationParserUtil.kt

--- android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
@@ -18,7 +18,7 @@ class LocationParserUtil {
                 isMocked = location.isFromMockProvider
             }
 
-            return hashMapOf<any>(
+            return hashMapOf<String, Any?>(
                     Keys.ARG_IS_MOCKED to isMocked,
                     Keys.ARG_LATITUDE to location.latitude,
                     Keys.ARG_LONGITUDE to location.longitude,
